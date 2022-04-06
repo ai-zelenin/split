f3742474937b4420e54a9417b14338d81ca3dcf7
AZ <ai.zelenin@yandex.ru>
2022-04-06 14:19:44 +0300

    add version;

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"go/ast"
 	"go/parser"
 	"go/token"
+	"os"
 )
 
 var srcDir = flag.String("src", "pb", "source dir package")
@@ -13,6 +15,14 @@ var dstDir = flag.String("dst", "pb_Sep", "source dir package")
 
 func main() {
 	flag.Parse()
+	arg := flag.Arg(0)
+	if arg != "" {
+		srcDir = &arg
+	}
+	if arg == "version" {
+		fmt.Println("0.1.0")
+		os.Exit(0)
+	}
 	sp := NewSegregatedPackage(*pkgName)
 	fset := token.NewFileSet()
 	pkgs, err := parser.ParseDir(fset, *srcDir, nil, parser.ParseComments)
